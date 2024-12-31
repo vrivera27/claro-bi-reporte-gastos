f74b1583abf0c46fa1fed4df4066201b05ef88dc
Victor Rivera <vrivera@attachconsultores.com>
2024-12-31 14:01:31 +0000

    [Add] conect budget to bigquery 11

--- functions/index.js
+++ functions/index.js
@@ -10,29 +10,18 @@ const DATASET_ID = "billing_reports";
 const TABLE_ID = "budget";
 
 exports.pushAllBudgetsToBigQuery = functions.https.onRequest((req, res) => {
-  // Usamos cors(...) directamente en el callback,
-  // sin hacer el bloque if (req.method === 'OPTIONS')...
-  return cors(req, res, async () => {
+  cors(req, res, async () => {
     try {
-      // Este array es el que envías desde fetch(...)
       const allBudgets = req.body;
+      functions.logger.info(`Presupuestos recibidos: ${allBudgets.length} documentos.`);
 
-      functions.logger.info(
-        `Presupuestos recibidos: ${allBudgets.length} documentos.`
-      );
-
-      // Borramos la tabla en BigQuery (full replace)
       try {
         await bigquery.dataset(DATASET_ID).table(TABLE_ID).delete();
         functions.logger.info(`Tabla ${TABLE_ID} eliminada en BigQuery (full replace).`);
       } catch (err) {
-        functions.logger.warn(
-          `No se pudo borrar la tabla (quizá no existe). Error: `,
-          err.message
-        );
+        functions.logger.warn(`No se pudo borrar la tabla (quizá no existe). Error: `, err.message);
       }
 
-      // Insertamos nuevamente
       const options = {
         ignoreUnknownValues: true,
         skipInvalidRows: true,
@@ -40,7 +29,10 @@ exports.pushAllBudgetsToBigQuery = functions.https.onRequest((req, res) => {
       await bigquery.dataset(DATASET_ID).table(TABLE_ID).insert(allBudgets, options);
       functions.logger.info(`Tabla ${TABLE_ID} recargada exitosamente en BigQuery.`);
 
-      // Devuelve OK (200)
+      res.set('Access-Control-Allow-Origin', '*');
+      res.set('Access-Control-Allow-Methods', 'GET, POST');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+
       return res.status(200).json({
         message: "Datos actualizados en BigQuery!",
         total: allBudgets.length
